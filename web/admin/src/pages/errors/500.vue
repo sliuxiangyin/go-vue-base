34fdf84448ec1708057f37f2016281986b636b6e
sliuxiangyin <s717563864@gmail.com>
2025-11-10 17:27:02 +0800

    update

---
+++ web/admin/src/pages/errors/500.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Error from '@/components/custom-error.vue'
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <Error
+      :code="500"
+      subtitle="Internal Server Error"
+      error="The server encountered an unexpected condition which prevented it from fulfilling the request."
+    />
+  </div>
+</template>
+
+<style scoped>
+</style>
+
+<route lang="yaml">
+  meta:
+    layout: blank
+  </route>
